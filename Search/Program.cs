fdd0500ed9721833478c39c61b4702690b64da07
كمبيولاب <@DESKTOP-V2DT3JS>
2024-12-31 11:41:45 +0200

    Add project files.

---
+++ Search/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+
+namespace Search
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
+            int X = 10;
+            int Y = 20;
+            Console.WriteLine($"Equation : {X} + {Y} = {X + Y:C}") ;
+        }
+    }
+}
